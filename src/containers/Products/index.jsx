2074fe7c55b725b6a8b7ba489666376e4e48bcdf
Joel Santos <joeltere9@gmail.com>
2023-01-27 15:47:36 -0300

    update products

--- src/containers/Products/index.jsx
+++ src/containers/Products/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import products from '../../assets/products.svg'
-import CategoryCarrosel from '../../components/CategoryCarrosel'
-import OffersCarousel from '../../components/OffersCarousel'
-import { ProductsContainer, ProductsImg } from './style'
+import api from '../../services/api'
+import { ProductsContainer, ProductsImg, CategoryButton,CategoriesMenu } from './style'
 
 function Products() {
+
+  const [categories, setCategories] = useState([])
+  const [activeCategory, setActiveCategory] = useState(0)
+
+console.log(activeCategory)
+    useEffect(() => {
+        const loadCategories = async () => {
+            const {data} = await api.get('categories')
+
+            const newCategories = [{id: 0 , name: 'Todas'}, ...data]
+          
+            setCategories(newCategories)
+        }
+        loadCategories()
+    } ,[])
   return (
     <ProductsContainer>
-        <ProductsImg src={products} alt="logo da home" />
-            
+
+<ProductsImg src={products} alt="logo da home" />
+      <CategoriesMenu>
+              {categories && categories.map(category => (
+                <CategoryButton isActiveCategory={ activeCategory === category.id} type='button' key={category.id} onClick={() => {setActiveCategory(category.id)}}>{category.name}</CategoryButton>
+              ))}
+                   </CategoriesMenu>
             </ProductsContainer>
   )
 }
--- src/containers/Products/style.jsx
+++ src/containers/Products/style.jsx
@@ -2,10 +2,32 @@ import styled from "styled-components";
 
 
 
-export const ProductsContainer = styled.div ``
+export const ProductsContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+`
 
 export const ProductsImg = styled.img `
 width: 100%;
 
+
+`
+export const CategoriesMenu = styled.div `
+    display: flex;
+  gap: 50px;
+   justify-content: center;
+   margin-top: 12px;
+`
+export const CategoryButton = styled.button`
+    cursor: pointer;
+    border: none;
+    background: none;
+    color: #9a9a9d;
+    font-family: 'Roboto';
+    color: ${props => props.isActiveCategory ? '#9758A5' : '#9a9a9d'};
+font-style: normal;
+font-weight: 400;
+font-size: 17px;
+line-height: 20px;
 
 `
